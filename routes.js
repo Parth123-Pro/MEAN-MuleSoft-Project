9aa52f3741442283a1c2a5bf8e532613cb1caa96
Parmar Parth <parmarparth@Parmars-MacBook-Pro.local>
2022-02-03 23:16:53 +0530

    f

--- routes.js
+++ routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const AddressI = require('./models/cryptos');
-const PersonI = require('./models/nfts');
+const Crypto = require('./models/cryptos');
+const Nft = require('./models/nfts');
 const req = require('express/lib/request');
 
 //crypto
 router.get('/cryptos', async (request, response) => {  
-    const data = await Crypto.find();
-    response.send(data);
+    const crypto = await Crypto.find();
+    response.send(crypto);
 });
 
 router.post('/cryptos', async (request, response) => {    
-    const crypto = new PersonI(request.body)
+    const crypto = new Crypto(request.body)
     await crypto.save();
     response.send(crypto);
 })
@@ -34,26 +34,26 @@ router.delete('/person/:name', async (request, response) => {
 })
 
 //nft
-router.get('/address', async (request, response) => {  // fetch
-    const data = await AddressI.find();
-    response.send(data);
+router.get('/nfts', async (request, response) => {  // fetch
+    const nft = await Nft.find();
+    response.send(nft);
 });
 
-router.post('/address', async (request, response) => {    // insert
-    const address = new AddressI(request.body)
+router.post('/nfts', async (request, response) => {    // insert
+    const nft = new Nft(request.body)
     address.save();
-    response.send(address);
+    response.send(nft);
 })
 
-router.patch('/address/:id', async (request, response) => {    // update
-    const _id = request.params.id;
-    const address = await AddressI.findByIdAndUpdate(_id, request.body, {new: true});
-    response.send(address);
+router.patch('/nfts/:name', async (request, response) => {    // update
+    const _id = request.params.name;
+    const nft = await Nft.findByIdAndUpdate(_id, request.body, {new: true});
+    response.send(nft);
 })
 
-router.delete('/address/:id', async (request, response) => {
-    const _id = request.params.id;
-    const address = await AddressI.findByIdAndDelete(_id);
-    response.send(address);
+router.delete('/nfts/:name', async (request, response) => {
+    const _id = request.params.name;
+    const nft = await Nft.findByIdAndDelete(_id);
+    response.send(nft);
 })
 module.exports = router;
