ad7862721a9d9ac672a338aca5d8586af3b6131e
Parmar Parth <parmarparth@Parmars-MacBook-Pro.local>
2022-02-04 00:08:03 +0530

    f

--- routes.js
+++ routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-var User = require('./Models/user')
-var bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken')
+
 const Crypto = require('./models/cryptos');
+const Nft = require('./models/nfts');
 const req = require('express/lib/request');
 
 //crypto
@@ -35,32 +34,26 @@ router.delete('/cryptos/:name', async (request, response) => {
 })
 
 //nft
-router.post('/users',async(req,res)=>{
-    
-  //generate salt key
-  salt = await bcrypt.genSalt(10)
-  console.log(salt)
+router.get('/nfts', async (request, response) => {  
+    const nft = await Nft.find();
+    response.send(nft);
+});
 
-  hashedpswd = await bcrypt.hash(req.body.password,salt)
-  console.log(hashedpswd)
-
-  const iuser = new User({
-      uname:req.body.uname,
-      password:hashedpswd
-  })  
-  await iuser.save((err,msg)=>{
-      if(err){
-          res.status(500).json({
-              "error":err
-          })
-      }
-      else{
-          res.status(200).json({
-              "My-message":msg
-          })
-      }
-  })
-
+router.post('/nfts', async (request, response) => {    
+    const nft = new Nft(request.body)
+    address.save();
+    response.send(nft);
 })
 
+router.patch('/nfts/:name', async (request, response) => {   
+    const _id = request.params.name;
+    const nft = await Nft.findByIdAndUpdate(_id, request.body, {new: true});
+    response.send(nft);
+})
+
+router.delete('/nfts/:name', async (request, response) => {
+    const _id = request.params.name;
+    const nft = await Nft.findByIdAndDelete(_id);
+    response.send(nft);
+})
 module.exports = router;
